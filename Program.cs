8f7c68413a958cd46c9c532829d9a9081769b3b6
Thalison jardim <thalisonjardim2@gmail.com>
2025-06-10 17:23:00 -0300

    Resolução do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,28 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+  static void Main()
+  {
+    Smartphone nokia = new Nokia("999999-999", "Windows Phone 5", "IMEI0989829382", 32);
+    Console.WriteLine("--- Testando Nokia---\n");
+    nokia.Ligar();
+    nokia.ReceberLigacao();
+    nokia.InstalarAplicativo("WhatsApp");
+
+
+    Smartphone iphone = new Iphone("9999999-9999", "Iphone 15", "IMEI 983232673458322", 128);
+    Console.WriteLine("\n---Testando Iphone---\n");
+    iphone.Ligar();
+    iphone.ReceberLigacao();
+    iphone.InstalarAplicativo("Telegram");
+
+    Console.WriteLine("\n---Testes Finalizados---\n");
+
+
+
+  }
+
+
+
+}
